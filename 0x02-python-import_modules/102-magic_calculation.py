eda80564c2ed569c116e615ca632b9feb7a8ea7f
yaaich <yaaichyounes96@gmail.com>
2023-08-11 19:38:34 +0100

    Python-Modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
+    add, sub = magic_calculation_102.add, magic_calculation_102.sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
+        return c
     else:
-        return(sub(a, b))
+        return sub(a, b)
+
+# Placeholder for magic_calculation_102 module with add and sub functions
+class magic_calculation_102:
+    @staticmethod
+    def add(a, b):
+        return a + b
+    @staticmethod
+    def sub(a, b):
+        return a - b

0bb6d7f529beffe185ef6e3fd9cc0632fc382e74
yaaich <yaaichyounes96@gmail.com>
2023-08-11 19:25:59 +0100

    python-module

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+from calculator_1 import add, subtract, multiply, divide
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = subtract(a, b)
+    elif operator == '*':
+        result = multiply(a, b)
+    elif operator == '/':
+        result = divide(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
+
+if __name__ == "__main__":
+    main()
